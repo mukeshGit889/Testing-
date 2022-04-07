90aa1afc055e65555adbe776b3f8ccf8c4578c14
Manash <Gmukesh>
2022-04-07 01:50:40 -0700

    THIRD push

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setC ontentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
     }
 }
